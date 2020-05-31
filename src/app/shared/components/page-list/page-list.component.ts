7e1b243092b545b431eba76cad98968fb4bf104d
FranklinParker <fparker@deliveryman.com>
2020-05-31 15:20:57 -0400

    fix

--- src/app/shared/components/page-list/page-list.component.ts
+++ src/app/shared/components/page-list/page-list.component.ts
@@ -1,15 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
   styleUrls: ['./page-list.component.scss']
 })
-export class PageListComponent implements OnInit {
+export class PageListComponent implements OnInit, OnChanges {
   @Input() searchResults: any;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('results:' + JSON.stringify(this.searchResults, null,2) );
   }
 
 }
